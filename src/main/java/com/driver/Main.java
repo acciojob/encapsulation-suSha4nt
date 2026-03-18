79beab54941c4b175848e8d9adaa36fe100780db
suSha4nt <heysusanta@gmail.com>
2026-03-18 20:01:25 +0530

    encapsulation sumbit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,15 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+//        RWOnly ex = new RWOnly("Mahesh");
+        RWOnly ex = new RWOnly();
+//      'name' has private access in 'enc.RWOnly'
+//      java: name has private access in enc.RWOnly
+//      Using getter
+        ex.setName("Susanta");
+        System.out.println( ex.getName());
+        ex.setName("Accio Job");
+        System.out.println( ex.getName());
+    }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,16 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String name;
+
+//    public RWOnly(String name) {
+//        this.name = name;
+//    }
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
